b7228411107b34b68bcf2920a65b3fdc3fa02910
MehmetCopurCE <mhmtcpr120@gmail.com>
2025-06-24 22:41:27 +0300

    Initial commit

---
+++ services/discovery/src/test/java/com/mcopur/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcopur.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
