ae51fbac9a4c5e3feaf77b14785eb2ac2bee2565
MehmetCopurCE <mhmtcpr120@gmail.com>
2025-07-01 00:13:06 +0300

    Implement the order line controller

--- services/order/src/main/java/com/mcopur/ecommerce/orderline/OrderLineRepository.java
+++ services/order/src/main/java/com/mcopur/ecommerce/orderline/OrderLineRepository.java
@@ -3,6 +3,10 @@ package com.mcopur.ecommerce.orderline;
 import com.mcopur.ecommerce.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
     OrderLine order(Order order);
+
+    List<OrderLine> findAllByOrderId(Integer orderId);
 }
